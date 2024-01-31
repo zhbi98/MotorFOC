dfd8a0a9055e4ac00b302b743afd93694b3b5fd2
zhbi98 <1523022952@qq.com>
2024-01-31 13:43:13 +0800

    新增工程配置，硬件验证

--- Firmware/main/main.c
+++ Firmware/main/main.c
@@ -1,4 +1,3 @@
-
 /**
  * @file main.c
  *
@@ -10,12 +9,102 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include "main.h"
+
+/**********************
+ *  STATIC PROTOTYPES
+ **********************/
+
+static void SystemClock_Config(void);
+static void Error_Handler(void);
 
 /**********************
  *   GLOBAL FUNCTIONS
  **********************/
 
+/**
+ * Initializes the device's core clock in preparation for startup.
+ * The initialization frequency is 168 MHZ.
+ */
 int32_t main()
 {
+  HAL_Init();
+  SystemClock_Config();
+
+  for (;;) {
+
+  }
+
 	return 0;
+}
+
+/**
+ * Initializes the device's core clock in preparation for startup.
+ * The initialization frequency is 168 MHZ.
+ * System Clock source = PLL (HSE)
+ * SYSCLK(Hz) = 168000000
+ * HCLK(Hz) = 168000000
+ */
+static void SystemClock_Config(void)
+{
+
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+
+	/**Configure the main internal regulator output voltage*/
+  __HAL_RCC_PWR_CLK_ENABLE();
+
+  /*The voltage scaling allows optimizing the power consumption when the device is 
+    clocked below the maximum system frequency, to update the voltage scaling value 
+    regarding system frequency refer to product datasheet.*/
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+	/**Initializes the CPU, AHB and APB busses clocks*/
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI|RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 4;
+  RCC_OscInitStruct.PLL.PLLN = 168;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 7;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+  	Error_Handler();
+  }
+
+	/**Initializes the CPU, AHB and APB busses clocks*/
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+							  |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
+  {
+  	Error_Handler();
+  }
+
+	/**Configure the Systick interrupt time*/
+  HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq()/1000);
+
+	/**Configure the Systick*/
+  HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+
+  /*SysTick_IRQn interrupt configuration*/
+  HAL_NVIC_SetPriority(SysTick_IRQn, 15, 0);
+}
+
+/**
+ * This function is executed in case of error occurrence.
+ */
+static void Error_Handler(void)
+{
+  /*Turn LED5 on*/
+  /*BSP_LED_On(LED5);*/
+  while(1)
+  {
+  }
 }
---
+++ Firmware/main/main.h
@@ -0,0 +1,19 @@
+/**
+ * @file main.h
+ *
+ */
+
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+/*********************
+ *      INCLUDES
+ *********************/
+
+#include "stm32f4xx_hal.h"
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+
+#endif /*__MAIN_H__*/
